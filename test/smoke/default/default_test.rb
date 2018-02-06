d8c8105de77de62bdf7a51930b5171fdb8bb7dea
Tech-A46 <tech-a46@Tech-A46s-MacBook-Air.local>
2018-02-06 12:13:02 +0000

    created a new node ChefDK

---
+++ test/smoke/default/default_test.rb
@@ -0,0 +1,41 @@
+# # encoding: utf-8
+
+# Inspec test for recipe node::default
+
+# The Inspec reference, with examples and extensive documentation, can be
+# found at http://inspec.io/docs/reference/resources/
+
+# unless os.windows?
+#   # This is an example test, replace with your own test.
+#   describe user('root'), :skip do
+#     it { should exist }
+#   end
+# end
+
+# nginx configure
+	describe package ('nginx') do
+		it {should be_installed}
+	end
+
+	describe service ('nginx') do
+		it {should be_running}
+		it {should be_enabled}
+	end
+# This is an example test, replace it with your own test.
+	describe port(80) do
+  		it { should be_listening }
+	end
+
+# redirect config
+	describe http('http://localhost', enable_remote_worker: true) do
+		its('status') { should cmp 502 }
+	end
+
+
+# nodejs configure
+	describe package ('nodejs') do
+		it {should be_installed}
+		its('version') {should match /6\../}
+	end
+
+
